db83e174f423286e8a113669aab3fbf9ff14104f
Selaron <selaron@github.com>
2022-06-26 22:50:02 +0200

    Added fridge model RS6GA884CSL

--- custom_components/smartthings/switch.py
+++ custom_components/smartthings/switch.py
@@ -146,7 +146,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                         )
                     ]
                 )
-            elif model in ("21K_REF_LCD_FHUB6.0", "ARTIK051_REF_17K"):
+            elif model in ("21K_REF_LCD_FHUB6.0", "ARTIK051_REF_17K", "RS6GA884CSL"):
                 switches.extend(
                     [
                         SamsungOcfSwitch(
